58bdcc65a9b20b429c98127c81e7b6ad41d8ee12
devendra1608 <151811312+devendra1608@users.noreply.github.com>
2026-01-10 11:56:40 +0530

    Authentication Routes Established

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ import {createServer} from "node:http";
 import mongoose from "mongoose";
 import { connectToSocket } from "./controllers/socketManager.js";
 import cors from "cors"
+import userRoutes from "./routes/users.routes.js";
+
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
@@ -11,6 +13,9 @@ app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit : "40kb"}));
 app.use(express.urlencoded({limit: "40kb", extended : true}));
+
+app.use("/api/v1/users",userRoutes);
+
 
 const start = async () => {
 
