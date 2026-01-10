6baefd6fe8e06adf294dd5fff2797c6dad98b0b1
devendra1608 <151811312+devendra1608@users.noreply.github.com>
2026-01-10 10:18:33 +0530

    Initialized backend

---
+++ Backend/src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {createServer} from "node:http";
+import mongoose from "mongoose";
+import { connectToSocket } from "./controllers/socketManager.js";
+import cors from "cors"
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000));
+app.use(cors());
+app.use(express.json({limit : "40kb"}));
+app.use(express.urlencoded({limit: "40kb", extended : true}));
+
+const start = async () => {
+
+  const connectionDB = await mongoose.connect(
+    "mongodb+srv://dbuser1:iamdbuser1@cluster0.jvu7nx3.mongodb.net/?appName=Cluster0"
+  );
+
+  console.log(`MONGODB Connected DB HOST : ${connectionDB.connection.host}`);
+
+  server.listen(app.get("port"), () => {
+    console.log("Listening on PORT 8000");
+  });
+};
+
+start();
